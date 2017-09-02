485c831feeb0bd4a4a3ab62e0aa76fa24a95c36c
unknown <Saiidma@gmail.com>
2017-09-02 12:14:38 +0100

    MenuProject

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Menu from './components/menu';
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Menu />
+  </View>
+);
+
+export default App;
